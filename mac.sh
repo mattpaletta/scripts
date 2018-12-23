ffaa070faacc70c3886ce347addccad0ca2f08ac
Matthew Paletta <mattpaletta@gmail.com>
2018-12-22 22:40:53 -0800

    Create mac.sh
    Creating first version of Mac script

---
+++ mac.sh
@@ -0,0 +1,19 @@
+set -e
+
+# Install Brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Cask install
+brew install cask watch watchman python3 cmake
+
+# Install docker?
+# brew install docker docker-compose docker-machine xhyve docker-machine-driver-xhyve
+# sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+# sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+# docker-machine create default --driver xhyve --xhyve-experimental-nfs-share
+
+# Pip3 install
+python3 get-pip.py
+
+# Get pip dependencies
+pip3 install virtualenv
