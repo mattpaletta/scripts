397a1b8c3016efc5445ee9f96f3acd62c23e2c02
Matthew Paletta <mattpaletta@gmail.com>
2019-01-06 19:54:30 -0800

    Added duplicate logic for constants file

--- tools/jetbrains.sh
+++ tools/jetbrains.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 set -e
-./constants.sh
+
+is_mac() {
+    return "$(uname)" == "Darwin"
+}
+
+is_linux() {
+    return "$(expr substr $(uname -s) 1 5)" == "Linux"
+}
+
+BASE_URL="https://raw.githubusercontent.com/mattpaletta/scripts/master/"
+
+exec_script() {
+    curl $1 | sh
+}
+
+run_platform() {
+    exec_script ${BASE_URL}/platform/$1.sh
+}
+
+run_tool() {
+    exec_script ${BASE_URL}/tools/$1.sh
+}
+
+run_tools() {
+    for script in $1
+    do
+        echo "Installing ${script}"
+        run_tool ${script}
+    done
+}
 
 if [[ is_mac ]]; then
     #brew cask install intellij-idea-ce pycharm-idea-ce
--- tools/swig.sh
+++ tools/swig.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
-./constants.sh
+
+is_mac() {
+    return "$(uname)" == "Darwin"
+}
+
+is_linux() {
+    return "$(expr substr $(uname -s) 1 5)" == "Linux"
+}
+
+BASE_URL="https://raw.githubusercontent.com/mattpaletta/scripts/master/"
+
+exec_script() {
+    curl $1 | sh
+}
+
+run_platform() {
+    exec_script ${BASE_URL}/platform/$1.sh
+}
+
+run_tool() {
+    exec_script ${BASE_URL}/tools/$1.sh
+}
+
+run_tools() {
+    for script in $1
+    do
+        echo "Installing ${script}"
+        run_tool ${script}
+    done
+}
 
 if [[ is_mac ]]; then
     # Do something under Mac OS X platform        
